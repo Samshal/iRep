7b715b0cc6bd662ffe1c1a0550736d6a272a686f
Hordunlarmy <Hordunlarmy@gmail.com>
2024-10-07 06:25:41 +0100

    feat(eye-witness-reports): Add eye witness report endpoints

--- irep-backend/database/migrations/2024_10_06_175357_eye_witness_report.php
+++ irep-backend/database/migrations/2024_10_06_175357_eye_witness_report.php
@@ -10,41 +10,39 @@ return new class () extends Migration {
     public function up(): void
     {
         DB::statement("
-            CREATE TABLE petitions (
+            CREATE TABLE eye_witness_reports (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 title VARCHAR(255) NOT NULL UNIQUE,
                 description TEXT NOT NULL,
                 creator_id INT NOT NULL,
-                target_representative_id INT NOT NULL,
-                signatures INT DEFAULT 0,
-                status ENUM('open', 'submitted', 'approved') DEFAULT 'open',
+                approvals INT DEFAULT 0,
+                category ENUM('crime', 'accident', 'other') DEFAULT 'other',
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE,
-                FOREIGN KEY (target_representative_id) REFERENCES representatives(id) ON DELETE CASCADE
+                FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE
             )
         ");
 
         DB::statement("
-            CREATE TABLE petition_signatures (
+            CREATE TABLE eye_witness_reports_approvals (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                petition_id INT NOT NULL,
+                report_id INT NOT NULL,
                 account_id INT NOT NULL,
-                signed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (petition_id) REFERENCES petitions(id) ON DELETE CASCADE,
+                approved_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (report_id) REFERENCES eye_witness_reports(id) ON DELETE CASCADE,
                 FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE,
-                UNIQUE (petition_id, account_id)
+                UNIQUE (report_id, account_id)
             )
-            ");
+        ");
 
         DB::statement("
-            CREATE TABLE petition_comments (
+            CREATE TABLE eye_witness_reports_comments (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                petition_id INT NOT NULL,
+                report_id INT NOT NULL,
                 account_id INT NOT NULL,
                 comment TEXT NOT NULL,
                 commented_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (petition_id) REFERENCES petitions(id) ON DELETE CASCADE,
+                FOREIGN KEY (report_id) REFERENCES eye_witness_reports(id) ON DELETE CASCADE,
                 FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE
             )
         ");
@@ -55,8 +53,8 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        DB::statement("DROP TABLE petition_comments");
-        DB::statement("DROP TABLE petition_signatures");
-        DB::statement("DROP TABLE petitions");
+        DB::statement("DROP TABLE eye_witness_reports_comments");
+        DB::statement("DROP TABLE eye_witness_reports_approvals");
+        DB::statement("DROP TABLE eye_witness_reports");
     }
 };
