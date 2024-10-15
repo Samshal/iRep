9d490451852d27926a0e68a8fe149d22f5a4e0dc
horduntech <horduntech@gmail.com>
2024-10-15 11:04:19 +0000

    .

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Replace DB_HOST and DB_PORT in the .env file with SERVICE_HOST and SERVICE_PORT
+sed -i "s/DB_HOST=.*/DB_HOST=database/" .env
+sed -i "s/DB_PORT=.*/DB_PORT=3306/" .env
+
 # Install Composer dependencies if not already installed
 if [ ! -d "vendor" ]; then
 	composer install
@@ -17,3 +21,4 @@ chown -R www-data:www-data storage bootstrap/cache
 
 # Run the Laravel application using artisan serve
 exec php artisan serve --host=0.0.0.0 --port=8000
+
